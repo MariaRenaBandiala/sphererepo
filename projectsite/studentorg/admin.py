3cbc08fc38abc42f874e71efa1a923f9e273ce9d
Maria Rena Bandiala <202280052@psu.palawan.edu.ph>
2025-04-08 16:42:13 +0800

    models and admin working properly and created initia data

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -1,12 +1,8 @@
-
-
 from django.contrib import admin
-
-# Register your models here.
 from .models import College, Program, Organization, Student, OrgMember
 
 admin.site.register(College)
 admin.site.register(Program)
 admin.site.register(Organization)
-admin.register(Student)
-admin.register(OrgMember)
+admin.site.register(Student)
+admin.site.register(OrgMember)
--- projectsite/studentorg/models.py
+++ projectsite/studentorg/models.py
@@ -1,6 +1,4 @@
-
 from django.db import models
-
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
@@ -49,4 +47,3 @@ class OrgMember(BaseModel):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
     date_joined = models.DateField()
-
